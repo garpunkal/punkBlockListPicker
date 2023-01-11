02e7b80af0afc66001a2556d0e45d975951d06b2
Gareth Wright <garpunkal@gmail.com>
2023-01-11 14:20:20 +0000

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# Content With Aside
+
+An app_plugin for Umbraco that allows you to view a Block List Picker with Layouts and Components split
+
+## Usage 
+
+Install and then setup block list blocks prefixed with "layout_" to appear in the layout section of the picker. 
+
+## Nuget
+
+`Install-Package punkBlockListPicker`
+
+https://www.nuget.org/packages/punkBlockListPicker/
+
+## Compatibility
+
+- Umbraco 10 with Block List Editor.
---
+++ punkBlockListPicker/App_Plugins/punkBlockListPicker/js/punkblocklistpicker.js
@@ -0,0 +1,16 @@
+﻿angular
+    .module('umbraco.services').config(['$httpProvider',
+        function ($httpProvider) {
+            $httpProvider.interceptors.push(
+                function ($q) {
+                    return {
+                        'request': function (request) {
+                            if (request.url.toLowerCase().indexOf('views/common/infiniteeditors/blockpicker/blockpicker.html') !== -1)
+                                request.url = '/app_plugins/tweaks/views/punkblocklistpicker.html';
+
+                            return request || $q.when(request);
+                        }
+                    };
+                });
+        }
+    ]);
